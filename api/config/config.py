102df35f1abcd659ea31bce030073c3a04e30a2f
Austin Wopara <austinwopara@gmail.com>
2023-02-23 03:39:23 +0100

    Correct DATABASE_URI configuration

--- api/config/config.py
+++ api/config/config.py
@@ -6,8 +6,8 @@ from datetime import timedelta
 BASE_DIR = os.path.dirname(os.path.realpath(__file__))
 
 uri = os.getenv('DATABASE_URL') # or other relevant config var
-if uri.startswith('postgres://'):
-    uri = uri.replace('postgres://', 'postgresql://', 1)
+# if uri.startswith('postgres://'):
+#     uri = uri.replace('postgres://', 'postgresql://', 1)
 
 conn = psycopg2.connect(uri, sslmode='require')
 
